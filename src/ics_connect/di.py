4c137b794efac840b38de7b7431684b5bc460b09
Austin Wagner <austinwagner@msn.com>
2025-11-04 18:41:03 -0800

    Add provide_repos with SQL or in-memory backends

--- src/ics_connect/di.py
+++ src/ics_connect/di.py
@@ -1,9 +1,29 @@
 from __future__ import annotations
 
+import os
 from collections.abc import Iterator
 
-from .db import Store, get_store
+from .db import Store, get_engine, get_session, get_store
+from .repositories.inmemory import InMemoryRepos
+from .repositories.protocols import Repos
+from .repositories.sql import SQLRepos
+from .repositories.sql_models import create_all as sql_create_all
 
 
 def provide_store() -> Iterator[Store]:
     yield from get_store()
+
+
+def provide_repos() -> Iterator[Repos]:
+    db_url = os.environ.get("DATABASE_URL")
+    if db_url:
+        engine = get_engine(db_url)
+        sql_create_all(engine)
+        for session in get_session(engine):
+            yield SQLRepos(session)
+        return
+    # In-memory repos wrapping the global Store
+    # Use the same store instance provided by provide_store/get_store
+    store_iter = get_store()
+    store = next(store_iter)
+    yield InMemoryRepos(store)
